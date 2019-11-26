e259108b7a68d7a25582aff4b1ced37f515aae7f
Mohamad Moghadam <moghadam@live.at>
2019-11-26 14:09:55 +0100

    final version

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,55 @@
+var path = require('path');
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: 'development',
+  entry: ['@babel/polyfill','./src/index.js'],
+  resolve: {
+    extensions: [".js", ".jsx", ".scss", ".css"]
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'bundle.js',
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: "/node-modules/",
+        
+        query: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          sourceMaps: true,
+          plugins: [
+            "@babel/plugin-proposal-class-properties"
+          ],
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve("./src/index.template.html"),
+      publicPath: "/",
+      filename: "index.html",
+      inject: true
+    })
+  ]
+};
+
+
+
+
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,47 +1,49 @@
 var path = require('path');
-
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  entry: ["@babel/polyfill", "./src/index.js"],
+  mode: 'production',
+  entry: ['@babel/polyfill','./src/index.js'],
   resolve: {
     extensions: [".js", ".jsx", ".scss", ".css"]
   },
   output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "bundle.js",
-    publicPath: '/'
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'bundle.js',
+    publicPath: "/"
+  
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         loader: "babel-loader",
-        exclude: "/node_modules/",
-        
+        exclude: "/node-modules/",
         query: {
           presets: ["@babel/preset-env", "@babel/preset-react"],
-          sourceMaps: true,
-          plugins: [
-            "@babel/plugin-proposal-class-properties"
-          ],
+          sourceMaps: true
         }
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
       }
     ]
   },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'public'),
-    port: 4000,
-    open: true
-  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.template.html",
+      template: path.resolve("./src/index.template.html"),
       publicPath: "/",
       filename: "index.html",
-      inject: true,
+      inject: false
     })
   ]
-  
 };
+
+
+
+
