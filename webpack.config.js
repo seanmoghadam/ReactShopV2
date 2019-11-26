9bcc1182ae120662ec697d67f5f6940264f3d76e
Mohamad Moghadam <moghadam@live.at>
2019-11-26 11:01:41 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require('path');
+
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: ["@babel/polyfill", "./src/index.js"],
+  resolve: {
+    extensions: [".js", ".jsx", ".scss", ".css"]
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: "/\.jsx?$/",
+        loader: "babel-loader",
+        exclude: "/node_modules/",
+  
+        query: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          sourceMaps: true,
+          plugins: [],
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.template.html",
+      publicPath: "/",
+      filename: "index.html",
+      inject: true
+    })
+  ]
+  
+};
