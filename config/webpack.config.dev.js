10447ba37b9125e7143940cb2604de490a03d882
Mohamad Moghadam <moghadam@live.at>
2019-11-26 15:55:01 +0100

    fixed issue with webpack config

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,50 @@
+var path = require('path');
+
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: ["@babel/polyfill", "./src/index.js"],
+  resolve: {
+    extensions: [".js", ".jsx", ".scss", ".css"]
+  },
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "bundle.js",
+    publicPath: "/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: "/node_modules/",
+  
+        query: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          sourceMaps: true,
+          plugins: [
+          
+          ],
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.template.html",
+      publicPath: "/",
+      filename: "index.html",
+      inject: true
+    })
+  ]
+  
+};
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -15,7 +15,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: "/\.jsx?$/",
+        test: /\.jsx?$/,
         loader: "babel-loader",
         exclude: "/node_modules/",
   
